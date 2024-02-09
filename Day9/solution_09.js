612b36551f8584548e7fe98a00a902d60f8fcda2
lucKy <luckydhinga123@gmail.com>
2024-02-09 16:00:49 +0530

    addes solution day09

---
+++ Day9/solution_09.js
@@ -0,0 +1,11 @@
+//         ---------------Answer of Today's Question----------------     
+//         d. All of the above
+// Express, Body-parser, and Socket.io are all Node.js modules that play crucial roles in web development, enabling developers to build scalable, feature-rich web applications and APIs with ease.
+// Express is a web application framework for Node.js. It provides a robust set of features for building web applications and APIs. With Express, you can easily define routes to handle different HTTP requests (GET, POST, PUT, DELETE, etc.)
+// Body-parser is a middleware for Express that extracts the body of an incoming request and makes it available in req.body. It supports various formats, including JSON and URL-encoded data.
+// Socket.io is a library that enables real-time, bidirectional communication between web clients and servers. It provides a simple API for creating WebSocket connections and handling events in real-time.
+
+//         -------------------------------------------------------------
+// Time: 0.000s
+// ------------------------------------------------------------
+// ------------------------------------------------------------
